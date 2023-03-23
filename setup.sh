c278f1bdb5b52ab78110c650284a5da2558a7e90
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2023-03-22 20:20:29 -0400

    submodule init for chesspursuit on 404 page

--- setup.sh
+++ setup.sh
@@ -8,6 +8,10 @@ for repo in "${repos[@]}"; do
     [ ! -d $repo ] && git clone https://github.com/lichess-org/$repo.git
 done
 
+cd lila
+git submodule update --init
+cd ..
+
 echo "Compiling js/css..."
 docker run --rm -v $(pwd):/mnt node:latest bash -c "npm install -g pnpm && /mnt/lila/ui/build"
 
