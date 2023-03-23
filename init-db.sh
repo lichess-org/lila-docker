8c023c6d4c53d314c6866792167206963fc889f4
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2023-03-22 20:20:52 -0400

    updated spamdb argument

--- init-db.sh
+++ init-db.sh
@@ -12,7 +12,7 @@ docker compose run --rm -v $(pwd)/lila:/lila mongodb bash -c \
     "mongo --host mongodb lichess /lila/bin/mongodb/indexes.js"
 
 docker compose run --rm python bash -c \
-    "pip install pymongo && python /lila-db-seed/spamdb/spamdb.py --uri=mongodb://mongodb/lichess --password=$password --su-password=$su_password --es --es-host=elasticsearch"
+    "pip install pymongo && python /lila-db-seed/spamdb/spamdb.py --uri=mongodb://mongodb/lichess --password=$password --su-password=$su_password --es --es-host=elasticsearch:9200"
 
 docker compose run --rm -v $(pwd)/scripts:/scripts mongodb bash -c \
     "mongosh --host mongodb lichess --file /scripts/mongodb/users.js"
