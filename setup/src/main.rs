55a42f178ba5c7eaa96fee04f05e3968a6dc5c45
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2023-09-02 22:21:49 -0400

    setup db automatically

--- setup/src/main.rs
+++ setup/src/main.rs
@@ -1,6 +1,6 @@
 use std::vec;
 
-use cliclack::{intro, multiselect};
+use cliclack::{confirm, input, intro, multiselect};
 
 const BANNER: &str = r#"
    |\_    _ _      _
@@ -27,9 +27,36 @@ fn main() -> std::io::Result<()> {
         .item("images", "Images (for generating gifs and thumbnails)", "")
         .interact()?;
 
+    let setup_database =
+        confirm("Do you want to seed the database with test users, games, etc?").interact()?;
+
+    let (su_password, password) = if setup_database {
+        let su_password: String = input("Choose a password for admin users (blank for 'password')")
+            .placeholder("password")
+            .default_input("password")
+            .required(false)
+            .interact()?;
+
+        let password: String = input("Choose a password for regular users (blank for 'password')")
+            .placeholder("password")
+            .default_input("password")
+            .required(false)
+            .interact()?;
+
+        (su_password, password)
+    } else {
+        (String::from(""), String::from(""))
+    };
+
     std::fs::write(
         "../.env",
-        format!("COMPOSE_PROFILES={}", profiles.join(",")),
+        format!(
+            "COMPOSE_PROFILES={}\nSETUP_DB={}\nSU_PASSWORD={}\nPASSWORD={}",
+            profiles.join(","),
+            setup_database,
+            su_password,
+            password
+        ),
     )?;
 
     Ok(())
