b340bedf17c5006f9e006ceafd71a29d363224e8
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2023-11-06 22:03:30 -0500

    update user output and have it work with mongodb 4

--- scripts/mongodb/users.js
+++ scripts/mongodb/users.js
@@ -3,23 +3,41 @@
 let users = db.user4
   .find()
   .toArray()
-  .filter((user) => user._id !== 'lichess') // `lichess` user login is disabled
+  .filter((user) => user._id !== "lichess") // `lichess` user login is disabled
   .map((user) => {
     return {
       username: user._id,
-      roles: user.roles.join(', '),
-      title: user.title || '',
-      marks: user.marks.join(', '),
-    }
-  })
+      roles: user.roles.join(", "),
+      title: user.title || "",
+      marks: user.marks.join(", "),
+    };
+  });
 
-console.log('Special User Accounts')
-console.table(users.filter((user) => user.roles))
+function printUsers(users) {
+  print(users.map((user) => user.username).join(", "));
+  print();
+}
 
-console.log('Marked Accounts')
-console.table(users.filter((user) => user.marks))
+print("==========================================");
+print("            Test User Accounts            ");
+print("==========================================");
 
-console.log('Regular Accounts')
-console.table(users.filter((user) => !user.roles && !user.marks))
+print("Special User Accounts");
+print("---------------------");
+printUsers(users.filter((user) => user.roles));
 
-console.log('You can log in with any of the above user accounts.')
+print("Marked Accounts");
+print("---------------");
+printUsers(users.filter((user) => user.marks));
+
+print("BOT Accounts");
+print("------------");
+printUsers(users.filter((user) => user.title === "BOT"));
+
+print("Regular Accounts");
+print("----------------");
+printUsers(
+  users.filter((user) => !user.roles && !user.marks && user.title !== "BOT"),
+);
+
+print("You can log in with any of the above user accounts.");
