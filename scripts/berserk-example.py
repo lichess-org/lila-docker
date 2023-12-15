1e67a592f8924077ced988f53433c745dac0bce1
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2023-12-15 17:53:50 -0500

    use preset token in sample api client script

--- scripts/berserk-example.py
+++ scripts/berserk-example.py
@@ -1,20 +1,8 @@
 import berserk
 import pprint
 
-session = None
-
-# # For authenticated requests, specify your API token:
-# # Create a token here: http://nginx/account/oauth/token/create?description=Test+Berserk+Token
-# my_token = 'lip_abc123'
-# session = berserk.TokenSession(my_token)
-
+session = berserk.TokenSession('lip_bobby')
 client = berserk.Client(session, base_url="http://nginx")
 
-# # Sample authenticated request:
-# print('\n#### Me ####\n')
-# me = client.account.get()
-# print(me)
-
-print('\n#### Get user profile ####\n')
-user = client.users.get_public_data("lichess")
-pprint.pprint(user)
+me = client.account.get()
+pprint.pprint(me)
