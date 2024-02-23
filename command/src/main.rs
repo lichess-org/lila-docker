3ffc33156ae8969981d5e39255fd0308c94afdfa
Carbrex <95964955+Carbrex@users.noreply.github.com>
2024-02-24 01:41:12 +0530

    Fetch the pr branch

--- command/src/main.rs
+++ command/src/main.rs
@@ -7,6 +7,7 @@ use cliclack::{
 };
 use local_ip_address::local_ip;
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 use std::{
     format,
     io::Error,
@@ -280,6 +281,58 @@ fn setup(mut config: Config) -> std::io::Result<()> {
         );
 
         progress.stop(format!("Cloned {} ✓", repo.full_name()));
+    }
+    
+    // checkout a pr branch in repos/lila
+    let mut cmd = std::process::Command::new("git");
+
+    // print all the env vars to the console
+    for (key, value) in std::env::vars() {
+        println!("{}: {}", key, value);
+    }
+
+    let workspace_context = match std::env::var("GITPOD_WORKSPACE_CONTEXT") {
+        Ok(value) => value,
+        Err(_) => {
+            return outro("Environment variable GITPOD_WORKSPACE_CONTEXT is not set, skipping PR checkout\n Starting services...");
+        }
+    };
+    
+    // parse the workspace context as JSON
+    let workspace_context: Value = serde_json::from_str(&workspace_context)
+        .expect("Failed to parse GITPOD_WORKSPACE_CONTEXT as JSON");
+    
+    // get the pr no from the workspace context
+    let pr_no = workspace_context.get("envvars").and_then(|envvars| {
+        envvars
+            .as_array()
+            .and_then(|array| {
+                array
+                    .iter()
+                    .find(|envvar| envvar.get("name").map_or(false, |name| name == "GITPOD_GIT_PR_NUMBER"))
+                    .and_then(|envvar| envvar.get("value").and_then(Value::as_str))
+            })
+    }).unwrap_or("");
+    
+    // dont checkout if the pr_no is empty
+    if pr_no.is_empty() {
+        return outro("No PR number found, skipping PR checkout\n Starting services...");
+        
+    }
+    cmd.current_dir("repos/lila")
+    .arg("fetch")
+    .arg("upstream")
+    .arg(format!("pull/{}/head:pr-{}", pr_no, pr_no));
+
+    let status = cmd.status().unwrap();
+    if !status.success() {
+        // If the checkout failed, checkout to the master branch
+        let mut cmd = std::process::Command::new("git");
+        cmd.current_dir("repos/lila")
+            .arg("checkout")
+            .arg("master")
+            .status()
+            .expect("Failed to checkout to master branch");
     }
 
     outro("Starting services...")
