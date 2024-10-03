b57d8875c7d6046319d4665b4807986706c64928
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2024-10-02 17:55:33 -0700

    Add script for when run using powershell (#126)

---
+++ lila-docker.ps1
@@ -0,0 +1,11 @@
+echo "
+   |\_    _ _      _
+   /o \  | (_) ___| |__   ___  ___ ___   ___  _ __ __ _
+ (_. ||  | | |/ __| '_ \ / _ \/ __/ __| / _ \| '__/ _` |
+   /__\  | | | (__| | | |  __/\__ \__ \| (_) | | | (_| |
+  )___(  |_|_|\___|_| |_|\___||___/___(_)___/|_|  \__, |
+                                                   |___/"
+
+echo "If you are trying to run lila-docker on Windows, you need to install and use Windows with WSL"
+echo "See the instructions at https://aka.ms/wslstore"
+echo ""
