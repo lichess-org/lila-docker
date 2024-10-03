f71365aeb3891f1dc8b48997487a78c385cd3def
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2024-10-02 20:53:30 -0400

    tweak output

--- lila-docker.ps1
+++ lila-docker.ps1
@@ -1,4 +1,5 @@
-echo "   |\_    _ _      _
+echo "
+   |\_    _ _      _
    /o \  | (_) ___| |__   ___  ___ ___   ___  _ __ __ _
  (_. ||  | | |/ __| '_ \ / _ \/ __/ __| / _ \| '__/ _` |
    /__\  | | | (__| | | |  __/\__ \__ \| (_) | | | (_| |
@@ -7,3 +8,4 @@ echo "   |\_    _ _      _
 
 echo "If you are trying to run lila-docker on Windows, you need to install and use Windows with WSL"
 echo "See the instructions at https://aka.ms/wslstore"
+echo ""
